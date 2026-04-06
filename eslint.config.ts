7dcb0bb50c0214f8c3ff6a5f69390928fa7ab5ff
kuankuan <2163826131@qq.com>
2026-04-06 15:43:40 +0800

    chore: update dependencies in package.json

--- eslint.config.ts
+++ eslint.config.ts
@@ -14,7 +14,7 @@ export default defineConfigWithVueTs(
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/temp/**']),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
