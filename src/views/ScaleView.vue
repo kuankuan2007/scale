bf82e9f37e140fc556758e981edee04fef5d4d77
kuankuan <2163826131@qq.com>
2026-09-06 15:24:50 +0800

    fix: 修复进度计算逻辑以支持副标题类型的问题

--- src/views/ScaleView.vue
+++ src/views/ScaleView.vue
@@ -163,7 +163,7 @@ const progress = computed(() => {
   let now = 0,
     total = 0;
   for (const i of data.value.questions) {
-    if (formData[i.id] !== void 0) {
+    if (formData[i.id] !== void 0 || i.form.type === 'subtitle') {
       now++;
     }
     total++;
